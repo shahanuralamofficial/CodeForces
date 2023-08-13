21f36285004b458d01322cd571026c66b1675313
Md. Shahanur Alam <108014474+shahanuralamofficial@users.noreply.github.com>
2023-08-13 17:11:34 +0600

    Update Way_Too_Long_Words.cpp

--- Way_Too_Long_Words.cpp
+++ Way_Too_Long_Words.cpp
@@ -1,6 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    string word;
+    double length;
+    cin >> word;
+    length = word.length();
+
+    cout << fixed << setprecision(0);
+    if (length > 10)
+    {
+        cout << word[0] << length - 2 << word[length - 1] << endl;
+    }
+
+    else
+    {
+        cout << word << endl;
+    }
+    return 0;
 }
