cbceaa5a93f8fb14043b4732028b5525a5a9e391
Md. Shahanur Alam <108014474+shahanuralamofficial@users.noreply.github.com>
2023-09-07 13:57:16 +0000

    By the mercy of ALLAH

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void solve()
+{
+    int n, result = 0;
+    cin >> n;
+
+    result = (n / 5);
+
+    if (result > 1)
+    {
+        result++;
+    }
+    cout << result << "\n";
+}
+
+int main()
+{
+    solve();
+
+    return 0;
+}
