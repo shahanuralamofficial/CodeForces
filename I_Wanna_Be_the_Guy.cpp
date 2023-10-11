397e83efe5bbd277b6e54b18b73a2536b0d1199d
Md. Shahanur Alam <108014474+shahanuralamofficial@users.noreply.github.com>
2023-10-11 19:59:56 +0600

    Add files via upload

--- I_Wanna_Be_the_Guy.cpp
+++ I_Wanna_Be_the_Guy.cpp
@@ -2,9 +2,52 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
 void solve()
 {
+    int level, n, m;
+
+    cin >> level;
+
+    vector<bool> canPass(level + 1, false);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int passedLevel;
+        cin >> passedLevel;
+        canPass[passedLevel] = true;
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int passedLevel;
+        cin >> passedLevel;
+        canPass[passedLevel] = true;
+    }
+
+    bool canPassAllLevels = true;
+
+    for (int i = 1; i <= level; i++)
+    {
+        if (!canPass[i])
+        {
+            canPassAllLevels = false;
+            break;
+        }
+    }
+
+    if (canPassAllLevels)
+    {
+        cout << "I become the guy." << endl;
+    }
+    else
+    {
+        cout << "Oh, my keyboard!" << endl;
+    }
 }
+
 int main()
 {
     solve();
